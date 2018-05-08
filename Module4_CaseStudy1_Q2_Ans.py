f7723aa48af8c15fe149cd7aea77c0608101657c
Diwakar Kumar <diwakarkumar.ind@gmail.com>
2018-05-08 16:37:15 +0530

    Extract CSV

--- Module4_CaseStudy1_Q2_Ans.py
+++ Module4_CaseStudy1_Q2_Ans.py
@@ -1,7 +1,13 @@
-# Extract data from the given CSV file and store the data from each column in a separate NumPy array
-
 import numpy as np
 path_to_csv='c:\\temp\\SalaryGender.csv'
 # arr = np.genfromtxt(path_to_csv, dtype=None, delimiter=',', skip_header=1)
 arr = np.loadtxt(path_to_csv, dtype=np.object, delimiter=",", skiprows=1)
-print arr
+# print arr
+count_of_phd=0
+temp=0
+for i in range(len(arr)):
+    temp = int(arr[i][3])
+    if temp==1:
+        count_of_phd=count_of_phd+temp
+
+print count_of_phd
