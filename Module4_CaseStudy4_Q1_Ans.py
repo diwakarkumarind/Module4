41c31e2b32f1bab80ce1b0ef996d7f85c65361ec
Diwakar Kumar <diwakarkumar.ind@gmail.com>
2018-05-12 21:15:11 +0530

    Plot a scatter graph for the monthly sales for BigMartData

--- Module4_CaseStudy4_Q1_Ans.py
+++ Module4_CaseStudy4_Q1_Ans.py
@@ -1,13 +1,15 @@
+#Plot Total Sales Per Month for Year 2011.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 dataset=pd.read_csv("c:\\temp\\BigMartSalesData.csv")
-selected_data=dataset.loc[:,['Month','Amount']]
+selected_data=dataset.loc[:,['Month','Quantity']]
 x=np.array(selected_data['Month'])
-y=np.array(selected_data['Amount'])
+y=np.array(selected_data['Quantity'])
 plt.scatter(x,y)
 plt.xlabel('Month of Sales')
-plt.ylabel('Total Amount of Sales')
+plt.ylabel('Total Sales')
 plt.grid()
+plt.savefig('C:\\Users\\Diwakar\\Pictures\\TotalSales2011.png')
 plt.show()
 
