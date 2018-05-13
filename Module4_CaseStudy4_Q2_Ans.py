f719f491024da251cba4c8d30bef8cafea71e43e
Diwakar Kumar <diwakarkumar.ind@gmail.com>
2018-05-13 14:50:44 +0530

    Initial commit

--- Module4_CaseStudy4_Q2_Ans.py
+++ Module4_CaseStudy4_Q2_Ans.py
@@ -4,13 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 dataset=pd.read_csv("c:\\temp\\BigMartSalesData.csv")
 selected_data=dataset.loc[:,['Month','Quantity']]
-x=np.array(selected_data['Month'])
-y=np.array(selected_data['Quantity'])
-plt.bar(x,y)
+x=np.array(selected_data["Month"])
+y=np.array(selected_data["Quantity"])
+x2=np.unique(x)
+y2=selected_data.groupby('Month')['Quantity'].count()
+df=pd.DataFrame(y2)
+y3=df['Quantity'].values.tolist()
+# print y3
+plt.bar(x2,y3)
 plt.xlabel('Month of Sales')
-plt.ylabel('Total Sales')
+plt.ylabel('Total Quantity Sales')
 plt.grid()
-plt.savefig('C:\\Users\\Diwakar\\Pictures\\TotalSales2011_Bar.png')
+plt.savefig('C:\\Users\\Diwakar\\Pictures\\BarChart001.png')
 plt.show()
 
 # Bar Chart isn't the better choice to visualize than Simple Plot
